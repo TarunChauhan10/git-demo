47ae5f91cb40b8890ddcd066267abd0e5f8b3045
Tarun Chauhan <k.tarunchauhan@gmail.com>
2022-06-25 00:35:03 +0530

    Adding Pattern_One.java program to the Git Hub

---
+++ Test_Practice/Pattern_One.java
@@ -0,0 +1,17 @@
+class PAttern_One
+{
+	public static void main(String[] args)
+	{
+		for(int i=0; i<5; i++)
+		{
+			for(int j=0; J<i; J++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("End of Program");
+	}
+}
+
